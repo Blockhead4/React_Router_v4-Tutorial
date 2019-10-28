28709e38b4123eaecc2c23d087d84c627dfbea0c
blockhead4 <blockhead4@naver.com>
2019-10-28 14:49:49 +0900

    Modify Router System and Style add

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -4,7 +4,11 @@ import { Redirect } from "react-router-dom";
 const logged = true;
 
 const MyPage = () => {
-  return <div>{!logged && <Redirect to="/login" />}마이페이지</div>;
+  return (
+    <div>
+      <div>{!logged && <Redirect to="/login" />}마이페이지</div>
+    </div>
+  );
 };
 
 export default MyPage;
--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -5,9 +5,20 @@ const Post = ({ match }) => {
   return <h2>{match.params.title}</h2>;
 };
 
-const Posts = () => {
+const Posts = ({ history }) => {
+  const handleClick = () => {
+    console.log(history.location.pathname);
+  };
+
+  const handleKeyPress = e => {
+    if (e.charCode === 13) handleClick();
+  };
+
   return (
     <div>
+      <input onKeyPress={handleKeyPress}></input>
+      <button onClick={handleClick}>버튼</button>
+
       <h1>포스트</h1>
       <Link to="/posts/react">React</Link>
       <Link to="/posts/redux">Redux</Link>
